d04584b235bfd25391a087e2a8e7aced343e8b21
Quiniou <quiniou@macbook-air-de-quiniou.home>
2023-07-11 17:10:31 +0200

    Added appartement by id

--- src/main/java/fr/qui/gestion/appart/AppartementController.java
+++ src/main/java/fr/qui/gestion/appart/AppartementController.java
@@ -5,10 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api", produces = "application/json")
 @CrossOrigin(origins = "https://gestion-front-7b998d8ddb5b.herokuapp.com")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class AppartementController {
     private final AppartementService appartementService;
 
@@ -19,6 +21,11 @@ public class AppartementController {
     @GetMapping("/appartements")
     public List<Appartement> obtenirTousLesAppartements() {
         return appartementService.obtenirTousLesAppartements();
+    }
+    
+    @GetMapping("/appartements/{id}")
+    public Optional<Appartement> obtenirUnAppartementParId(@PathVariable("id") Long id) {
+        return appartementService.findById(id);
     }
     
 
