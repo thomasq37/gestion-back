35e13dcd4aa6874b9df51c0919bfc08922e996a8
Quiniou <quiniou@macbook-air-de-quiniou.home>
2023-07-12 17:34:11 +0200

    Added calcul vacance locative

--- src/main/java/fr/qui/gestion/appart/AppartementController.java
+++ src/main/java/fr/qui/gestion/appart/AppartementController.java
@@ -1,15 +1,11 @@
 package fr.qui.gestion.appart;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,32 +20,49 @@ public class AppartementController {
     public AppartementController(AppartementService appartementService) {
         this.appartementService = appartementService;
     }
+    
     @GetMapping("/list")
     public List<Appartement> obtenirTousLesAppartements() {
         return appartementService.obtenirTousLesAppartements();
     }
     
     @GetMapping("/{id}")
-    public Optional<Appartement> obtenirUnAppartementParId(@PathVariable("id") Long id) {
-        return appartementService.findById(id);
+    public ResponseEntity<Appartement> obtenirUnAppartementParId(@PathVariable("id") Long id) {
+        try {
+            Appartement appartement = appartementService.obtenirUnAppartementParId(id);
+            return ResponseEntity.ok(appartement);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
-    
 
-    @PostMapping("/{id}/calcul-rentabilite")
-    public ResponseEntity<Double> calculerRentabiliteNette(@PathVariable("id") Long id) throws ClassNotFoundException {
-        double rentabiliteNette = appartementService.calculerRentabiliteNette(id);
-        return ResponseEntity.ok(rentabiliteNette);
+    @GetMapping("/{id}/calcul-rentabilite")
+    public ResponseEntity<Double> calculerRentabiliteNette(@PathVariable("id") Long id) {
+        try {
+            double rentabiliteNette = appartementService.calculerRentabiliteNette(id);
+            return ResponseEntity.ok(rentabiliteNette);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
     
-    @PostMapping("/{id}/moyenne-benefices")
-    public ResponseEntity<Double> calculerMoyenneBenefices(@PathVariable("id") Long id) throws ClassNotFoundException {
-        double moyenneBenefices = appartementService.calculerMoyenneBenefices(id);
-        return ResponseEntity.ok(moyenneBenefices);
+    @GetMapping("/{id}/moyenne-benefices")
+    public ResponseEntity<Double> calculerMoyenneBenefices(@PathVariable("id") Long id) {
+        try {
+            double moyenneBenefices = appartementService.calculerMoyenneBenefices(id);
+            return ResponseEntity.ok(moyenneBenefices);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
     
     @GetMapping("/{id}/taux-vacances-locatives")
-    public  ResponseEntity<Double> calculerTauxVacanceLocativeMoyen(@PathVariable("id") Long id) throws ClassNotFoundException {
-        double tauxVacancesLocatives = appartementService.calculerTauxVacancesLocatives(id);
-        return ResponseEntity.ok(tauxVacancesLocatives);
+    public ResponseEntity<Double> calculerTauxVacancesLocativeMoyen(@PathVariable("id") Long id) {
+        try {
+            double tauxVacancesLocatives = appartementService.calculerTauxVacancesLocatives(id);
+            return ResponseEntity.ok(tauxVacancesLocatives);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/fr/qui/gestion/appart/AppartementService.java
+++ src/main/java/fr/qui/gestion/appart/AppartementService.java
@@ -15,7 +15,6 @@ import fr.qui.gestion.mouvementappart.MouvementAppartementRepository;
 @Service
 public class AppartementService {
     private final AppartementRepository appartementRepository;
-    private final MouvementAppartementRepository mouvementAppartementRepository;
     
     @Autowired
     public AppartementService(
@@ -23,86 +22,70 @@ public class AppartementService {
     		MouvementAppartementRepository mouvementAppartementRepository) 
     {
         this.appartementRepository = appartementRepository;
-        this.mouvementAppartementRepository = mouvementAppartementRepository;
     }
+    
     public List<Appartement> obtenirTousLesAppartements() {
         return appartementRepository.findAll();
     }
     
-
-    public Optional<Appartement> findById(Long id) {
+    public Appartement obtenirUnAppartementParId(Long id) throws IllegalArgumentException {
         Optional<Appartement> optionalAppartement = appartementRepository.findById(id);
-        return optionalAppartement;
+        return optionalAppartement.orElseThrow(() -> new IllegalArgumentException("Appartement not found with ID: " + id));
     }
     
-    public double calculerRentabiliteNette(Long id) throws ClassNotFoundException {
-        Optional<Appartement> optionalAppartement = findById(id);
+    public double calculerRentabiliteNette(Long id) {
+        
+            Appartement appartement = findById(id);
 
-        if (optionalAppartement.isPresent()) {
-            Appartement appartement = optionalAppartement.get();
             double depensesTotales = 0.0;
             for (Frais frais : appartement.getFrais()) {
                 double montantAnnuelEquivalent = frais.getFrequence().convertirMontantAnnuel(frais.getMontant());
                 depensesTotales += montantAnnuelEquivalent;
             }
-            
+
             double rentabiliteNette = ((appartement.getLoyerMensuel() * 12 - depensesTotales) / appartement.getPrix()) * 100;
             return rentabiliteNette;
-        } else {
-            throw new ClassNotFoundException("Appartement non trouvé pour l'ID : " + id);
-        }
+        
     }
-    
-    public int calculerMoyenneBenefices(Long id) throws ClassNotFoundException {
 
-        Optional<Appartement> optionalAppartement = findById(id);
+    public int calculerMoyenneBenefices(Long id){
+            Appartement appartement = findById(id);
 
-        if (optionalAppartement.isPresent()) {
-            Appartement appartement = optionalAppartement.get();
             double depensesTotales = 0.0;
             for (Frais frais : appartement.getFrais()) {
                 double montantAnnuelEquivalent = frais.getFrequence().convertirMontantAnnuel(frais.getMontant());
                 depensesTotales += montantAnnuelEquivalent;
             }
-            
+
             int moyenneBenefices = (int) ((appartement.getLoyerMensuel() * 12 - depensesTotales) / 12);
             return moyenneBenefices;
-        } else {
-            throw new ClassNotFoundException("Appartement non trouvé pour l'ID : " + id);
-        }
+        
     }
 
-    public double calculerTauxVacancesLocatives(Long appartementId) {
-        Appartement appartement = appartementRepository.findById(appartementId)
-                .orElseThrow(() -> new IllegalArgumentException("Appartement non trouvé"));
-
-        List<MouvementAppartement> mouvements = appartement.getMouvements();
-        
-        // Assurez-vous que la liste est triée par date
-        mouvements.sort(Comparator.comparing(MouvementAppartement::getDate));
-        
-        // Initialize le total de jours et les jours de vacances à zéro
-        long totalJours = 0;
-        long joursVacances = 0;
-
-        // Parcourir tous les mouvements
-        for (int i = 0; i < mouvements.size() - 1; i++) {
-            MouvementAppartement courant = mouvements.get(i);
-            MouvementAppartement suivant = mouvements.get(i+1);
-
-            long joursEntre = ChronoUnit.DAYS.between(courant.getDate(), suivant.getDate());
-            totalJours += joursEntre;
-
-            // Si le mouvement actuel est une sortie (c'est-à-dire l'appartement est vacant),
-            // alors ajouter le nombre de jours entre les deux mouvements aux jours de vacances
-            if (!courant.isEstEntree()) {
-                joursVacances += joursEntre;
-            }
-        }
-
-        if (totalJours == 0) return 0.0;
-        double tauxVacances = (double) joursVacances / totalJours * 100;
-        double roundedTauxVacances = Math.round(tauxVacances * 100.0) / 100.0;
-        return roundedTauxVacances;
-    }
+	public double calculerTauxVacancesLocatives(Long appartementId) {
+	    Appartement appartement = findById(appartementId);
+	
+	    List<MouvementAppartement> mouvements = appartement.getMouvements();
+	
+	    mouvements.sort(Comparator.comparing(MouvementAppartement::getDate));
+	
+	    long totalJours = 0;
+	    long joursVacances = 0;
+	
+	    for (int i = 0; i < mouvements.size() - 1; i++) {
+	        MouvementAppartement courant = mouvements.get(i);
+	        MouvementAppartement suivant = mouvements.get(i+1);
+	
+	        long joursEntre = ChronoUnit.DAYS.between(courant.getDate(), suivant.getDate());
+	        totalJours += joursEntre;
+	
+	        if (!courant.isEstEntree()) {
+	            joursVacances += joursEntre;
+	        }
+	    }
+	
+	    if (totalJours == 0) return 0.0;
+	    double tauxVacances = (double) joursVacances / totalJours * 100;
+	    return Math.round(tauxVacances * 100.0) / 100.0;
+	}
 }
