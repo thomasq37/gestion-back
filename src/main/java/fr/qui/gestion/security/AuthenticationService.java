9540cfbe8478bce76b05f76d948db727671f72ed
Quiniou <quiniou@MacBook-Air-de-Quiniou.local>
2023-09-21 08:55:29 +0200

    Added authent rest api

---
+++ src/main/java/fr/qui/gestion/security/AuthenticationService.java
@@ -0,0 +1,27 @@
+package fr.qui.gestion.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+
+
+public class AuthenticationService {
+
+	@Value("${api.key}")
+	private static String apiKey;
+    private static final String AUTH_TOKEN_HEADER_NAME = "X-API-KEY";
+    private static final String AUTH_TOKEN = apiKey;
+
+    public static Authentication getAuthentication(HttpServletRequest request) {
+        String apiKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
+        if (apiKey == null || !apiKey.equals(AUTH_TOKEN)) {
+            throw new BadCredentialsException("Invalid API Key");
+        }
+
+        return new ApiKeyAuthentication(apiKey, AuthorityUtils.NO_AUTHORITIES);
+    }
+}
