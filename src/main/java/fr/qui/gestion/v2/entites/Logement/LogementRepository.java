359971ba674febdee7f519381ab789d2d6c084ef
Thomas Quiniou <thomasquiniou@MacBook-Air-de-Thomas.local>
2025-01-20 16:18:40 +0100

    Ajustement findByLogements -> dateAchat recent en premier

--- src/main/java/fr/qui/gestion/v2/entites/Logement/LogementRepository.java
+++ src/main/java/fr/qui/gestion/v2/entites/Logement/LogementRepository.java
@@ -2,6 +2,7 @@ package fr.qui.gestion.v2.entites.Logement;
 
 import fr.qui.gestion.v2.entites.Utilisateur.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.Optional;
 
 @Repository
 public interface LogementRepository extends JpaRepository<Logement, Long> {
+    @Query("SELECT l FROM Logement l JOIN l.caracteristiques c WHERE l.proprietaire = :proprietaire ORDER BY c.dateAchat DESC")
     List<Logement> findByProprietaire(Utilisateur proprietaire);
 
     Optional<Logement> findByMasqueId(String masqueId);
