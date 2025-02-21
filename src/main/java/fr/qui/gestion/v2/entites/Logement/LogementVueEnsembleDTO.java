281c27f319926c80131322ea92cfcdde836f3bd6
Thomas Quiniou <thomasquiniou@MacBook-Air-de-Thomas.local>
2025-02-21 15:57:52 +0100

    Ajout logements vue d'ensemble pour performances

--- src/main/java/fr/qui/gestion/v2/entites/Logement/LogementVueEnsembleDTO.java
+++ src/main/java/fr/qui/gestion/v2/entites/Logement/LogementVueEnsembleDTO.java
@@ -3,13 +3,8 @@ package fr.qui.gestion.v2.entites.Logement;
 import fr.qui.gestion.v2.entites.Adresse.AdresseDTO;
 import fr.qui.gestion.v2.entites.Alerte.AlerteDTO;
 import fr.qui.gestion.v2.entites.Caracteristiques.CaracteristiquesDTO;
-import fr.qui.gestion.v2.entites.Contact.ContactDTO;
-import fr.qui.gestion.v2.entites.Frais.FraisDTO;
 import fr.qui.gestion.v2.entites.PeriodeDeLocation.PeriodeDeLocationDTO;
 import fr.qui.gestion.v2.entites.Photo.PhotoDTO;
-import fr.qui.gestion.v2.entites.Utilisateur.Utilisateur;
-import fr.qui.gestion.v2.entites.Utilisateur.UtilisateurDTO;
-import fr.qui.gestion.v2.entites.document.DocumentDTO;
 import lombok.Data;
 
 import java.util.List;
